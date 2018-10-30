526f2713fe335d170b318bcccf40c387335a150f
Dustin Christensen <39143972+Christensen-Dustin@users.noreply.github.com>
2018-10-30 00:06:26 -0700

    Add files via upload

---
+++ css3TransitionsAndAnimations.js
@@ -0,0 +1,25 @@
+// Card Flip
+function flipIT(){
+    document.getElementById("flip").className = "flip";
+}
+
+function stopFlipIT(){
+    document.getElementById("flip").className = "c";
+}
+
+// Moving shapes
+function travel1() {
+    document.getElementById("ch1").style.animationName = "travel1";
+}
+function travel2() {
+    document.getElementById("ch2").style.animationName = "travel2";
+}
+function travel3() {
+    document.getElementById("ch3").style.animationName = "travel3";
+}
+function travel4() {
+    document.getElementById("ch4").style.animationName = "travel4";
+}
+function travel5() {
+    document.getElementById("ch5").style.animationName = "travel5";
+}
